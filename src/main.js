48ee3fb9c5822dab7517c4db173a8fc3afd620ef
Otto <ottoadrianobecker@gmail.com>
2024-09-16 22:01:27 -0300

    implementa funcoes

--- src/main.js
+++ src/main.js
@@ -36,8 +36,129 @@ app.get('/obtemImovelVenda', (req, res) => {
             res.status(200);
             res.setHeader('Content-Type', 'application/json');
             addCorsHttpHeaders(res);
-            console.log(resultados)
-            res.send(JSON.stringify(resultados));            
+            console.log(resultados);
+            res.send(JSON.stringify(resultados));
+        }
+    })
+
+});
+
+app.get('/obtemVendaPorImovel', (req, res) => {
+    let sql = ` SELECT 
+                    venda.id_venda as idVenda,
+                    DATE_FORMAT(venda.data_do_pagamento, '%d/%m/%Y') as dataPagamento,
+                    venda.valor_do_pagamento as valorPagamento,
+                    imovel.id_imovel as idImovel,
+                    imovel.descricao_imovel as descricaoImovel,
+                    imovel.tipo_imovel as tipoImovel
+                FROM venda
+                    LEFT JOIN imovel on imovel.ID_IMOVEL = venda.ID_IMOVEL`
+    
+    conn.query(sql, (erro, resultados) => {
+        if (erro) {
+            res.status(500);
+            res.send(JSON.stringify(erro));
+            console.log(erro);
+        } else {
+            res.status(200);
+            res.setHeader('Content-Type', 'application/json');
+            addCorsHttpHeaders(res);
+            const somaPagamentos = resultados.reduce((acc, pagamento) => {
+
+                if (!acc[pagamento.idImovel]) {
+                    acc[pagamento.idImovel] = 0; // Inicializa
+                }
+
+                acc[pagamento.idImovel] += pagamento.valorPagamento; // Soma
+                return acc
+            }, {});
+            console.log(somaPagamentos);
+            res.send(JSON.stringify(somaPagamentos));
+        }
+    })
+
+});
+
+app.get('/obtemVendaPorData', (req, res) => {
+    let sql = ` SELECT 
+                    venda.id_venda as idVenda,
+                    DATE_FORMAT(venda.data_do_pagamento, '%m/%Y') as dataPagamento,
+                    venda.valor_do_pagamento as valorPagamento,
+                    imovel.id_imovel as idImovel,
+                    imovel.descricao_imovel as descricaoImovel,
+                    imovel.tipo_imovel as tipoImovel
+                FROM venda
+                    LEFT JOIN imovel on imovel.ID_IMOVEL = venda.ID_IMOVEL`
+    
+    conn.query(sql, (erro, resultados) => {
+        if (erro) {
+            res.status(500);
+            res.send(JSON.stringify(erro));
+            console.log(erro);
+        } else {
+            res.status(200);
+            res.setHeader('Content-Type', 'application/json');
+            addCorsHttpHeaders(res);
+            const somaPagamentos = resultados.reduce((acc, pagamento) => {
+
+                if (!acc[pagamento.dataPagamento]) {
+                    acc[pagamento.dataPagamento] = 0; // Inicializa
+                }
+
+                acc[pagamento.dataPagamento] +=pagamento.valorPagamento; // Soma
+                return acc
+            }, {});
+            console.log(somaPagamentos);
+            res.send(JSON.stringify(somaPagamentos));
+        }
+    })
+
+});
+
+app.get('/obtemVendaQuantitativa', (req, res) => {
+    let sql = ` SELECT 
+                    venda.id_venda as idVenda,
+                    DATE_FORMAT(venda.data_do_pagamento, '%m/%Y') as dataPagamento,
+                    venda.valor_do_pagamento as valorPagamento,
+                    imovel.id_imovel as idImovel,
+                    imovel.descricao_imovel as descricaoImovel,
+                    imovel.tipo_imovel as tipoImovel
+                FROM venda
+                    LEFT JOIN imovel on imovel.ID_IMOVEL = venda.ID_IMOVEL`
+    
+    conn.query(sql, (erro, resultados) => {
+        if (erro) {
+            res.status(500);
+            res.send(JSON.stringify(erro));
+            console.log(erro);
+        } else {
+            res.status(200);
+            res.setHeader('Content-Type', 'application/json');
+            addCorsHttpHeaders(res);
+            const pagamentosTotais = resultados.reduce((acc, pagamento) => {
+                if(!acc){
+                    acc = 0;
+                }
+                acc += pagamento.valorPagamento
+                return acc;
+            }, 0);
+            const imoveisVendaQuantitativa = resultados.reduce((acc, pagamento) => {
+                if(!acc[pagamento.descricaoImovel]){
+                    acc[pagamento.descricaoImovel] = 0;
+                }
+                acc[pagamento.descricaoImovel] += pagamento.valorPagamento;
+                return acc;
+
+            }, {});
+            const imoveisVendaPorcentagem = Object.keys(imoveisVendaQuantitativa).reduce((acc, tipo) => {
+                const valor = imoveisVendaQuantitativa[tipo];
+                const percentual = ((valor / pagamentosTotais) * 100).toFixed(2) + '%';
+                acc[tipo] = percentual;
+                return acc;
+            }, {});
+
+            console.log(imoveisVendaPorcentagem);
+            res.send(JSON.stringify(imoveisVendaPorcentagem));
         }
     })
 
